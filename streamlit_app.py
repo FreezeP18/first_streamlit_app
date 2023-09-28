b072345728c1a25023d191d66db6e695b4ad8cab
FreezeP18 <41484979+FreezeP18@users.noreply.github.com>
2023-09-28 16:22:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ except URLError as e:
     streamlit.error()
 
 
-#dont run anything past here while trouble
-streamlit.stop()
+
+
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
